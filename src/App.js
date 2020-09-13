c08f3e0693001e0b8208e5b99f8472537ca33122
sujata <gunalesujata@gmail.com>
2020-09-13 18:06:30 +0530

    bank layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import formatNumber from "format-number";
 import photgrapher from "./assets/girl.png";
 
 class App extends Component {
@@ -14,7 +15,17 @@ class App extends Component {
       <div className="App">
         <img className="App__userpic" src={photgrapher} alt="user" />
         <p className="App__username">Hello, {username}!</p>
-        <div className="App_amount"></div>
+        <div className="App_amount">
+          {formatNumber({ prefix: "$" })(totalAmount)}
+          <p className="App__amount--info">Total Amount</p>
+        </div>
+
+        <section className="App__buttons">
+          <button data-amount="10000">Withdraw $10,000</button>
+          <button data-amount="5000">Withdraw $5000</button>
+        </section>
+
+        <p className="App_giveaway">Give away all your cash to charity</p>
       </div>
     );
   }
