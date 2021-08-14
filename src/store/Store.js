f53a6614313ee84cc217724eee84cdb341430cd0
Tidbits JS <tidbitsjs@gmail.com>
2021-08-15 00:06:29 +0530

    custom logger

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,7 +1,27 @@
 import { createStore, applyMiddleware } from "redux";
 import reducer from "../reducers/Reducer";
+
+/* Default Logger  
 import logger from "redux-logger";
-
 const middlewares = [logger];
+*/
 
-export const store = createStore(reducer, applyMiddleware(...middlewares));
+import { createLogger } from "redux-logger";
+
+// Custom logger
+const logger = createLogger({
+  collapsed: (getState, action) => action.type === "DONATE",
+  // predicate: (getState, action) => action.type === "CREDIT",
+  diff: true,
+  duration: true,
+  timestamp: true,
+  colors: {
+    title: () => "#0f1842",
+    prevState: () => "#de6f0d",
+    action: () => "#6e13ab",
+    nextState: () => "#1a9134",
+  },
+  // level: console.warn,
+});
+
+export const store = createStore(reducer, applyMiddleware(logger));
